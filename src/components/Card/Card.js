e826288a21eea82240004acc5f1149e126e633f2
seixeiro <nathan.seixeiro@gmail.com>
2023-08-08 00:20:27 -0300

    add mansory gallery layout

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+export function Card({ color, width, aspectRatio }){
+    return (
+      <View
+        style={{
+          backgroundColor: color,
+          width: width,
+          height: width * aspectRatio,
+          marginBottom: 10,
+          borderRadius: 10
+        }}></View>
+    );
+  };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,108 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 
 import { Logo } from '../components/Logo/Logo';
 import { InputSearch } from '../components/InputSearch/InputSearch';
+import { Card } from '../components/Card/Card';
+
+const images= [
+  {
+    id: 1,
+    aspectRatio: 150 / 200,
+    color: "#ffcdd2",
+  },
+  {
+    id: 2,
+    aspectRatio: 1,
+    color: "#bbdefb",
+  },
+  {
+    id: 3,
+    aspectRatio: 120 / 100,
+    color: "#c8e6c9",
+  },
+  {
+    id: 4,
+    aspectRatio: 200 / 150,
+    color: "#ffccbc",
+  },
+  {
+    id: 5,
+    aspectRatio: 375 / 400,
+    color: "#d1c4e9",
+  },
+  {
+    id: 6,
+    aspectRatio: 500 / 400,
+    color: "#bbdefb",
+  },
+  {
+    id: 7,
+    aspectRatio: 1040 / 500,
+    color: "#ffcdd2",
+  },
+  {
+    id: 8,
+    aspectRatio: 2 / 1,
+    color: "#bbdefb",
+  },
+  {
+    id: 9,
+    aspectRatio: 1,
+    color: "#c8e6c9",
+  },
+  {
+    id: 10,
+    aspectRatio: 375 / 400,
+    color: "#ffccbc",
+  },
+  {
+    id: 11,
+    aspectRatio: 1,
+    color: "#d1c4e9",
+  },
+];
 
 export function Home({navigation}) {
+  const width = Dimensions.get('window').width / 2;
   return (
     <SafeAreaView style={style.container}>
-        <Logo />
     <View style={style.header}>
-        <InputSearch />
+        <Logo />
+        {/* <InputSearch /> */}
         {/* teclado sobre junto com tabs*/}
     </View>  
+
+    {/* gallery */}
+    <ScrollView>
+          <View style={style.gallery}> 
+            <View style={{marginRight: 5}}>
+              {images.filter((i) => i.id % 2 === 0).map((img) => (
+                <Card
+                  key={img.id}
+                  color={img.color}
+                  aspectRatio={img.aspectRatio}
+                  width={width}
+                />
+              ))}
+            </View>
+            <View style={{marginLeft: 5}}>
+              {images.filter((i) => i.id % 2 !== 0).map((img) => (
+                <Card
+                  key={img.id}
+                  color={img.color}
+                  aspectRatio={img.aspectRatio}
+                  width={width}
+                />
+              ))}
+            </View>
+          </View>
+        </ScrollView>
     </SafeAreaView>
   );
 }
@@ -20,12 +111,12 @@ const style = StyleSheet.create({
     container: {
         alignItems: 'center',
         justifyContent: 'center',
-        paddingTop: 35,
-        paddingLeft: 14,
-        paddingRight: 14,
-        marginRight: 30
-    },
+        padding: 10,
+      },
     header:{
-      left: 20
+      paddingTop: 35,
     },
+    gallery: {
+      flexDirection: 'row'
+    }
 })
